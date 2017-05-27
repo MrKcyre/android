c7aa09c4d7d9773821872e05b13397b04886d0ee
AndyScherzinger <info@andy-scherzinger.de>
2017-05-28 01:31:14 +0200

    correctly persist newly created/configured custom folder and refresh list

--- src/main/java/com/owncloud/android/ui/adapter/FolderSyncAdapter.java
+++ src/main/java/com/owncloud/android/ui/adapter/FolderSyncAdapter.java
@@ -77,6 +77,11 @@ public class FolderSyncAdapter extends SectionedRecyclerViewAdapter<FolderSyncAd
         notifyDataSetChanged();
     }
 
+    public void addSyncFolderItem(SyncedFolderDisplayItem syncFolderItem) {
+        mSyncFolderItems.add(syncFolderItem);
+        notifyDataSetChanged();
+    }
+
     @Override
     public int getSectionCount() {
         return mSyncFolderItems.size();
